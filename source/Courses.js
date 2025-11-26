f2bfa7cdc649c61386bf26aec6bbee4391b25797
aduc78875-sketch <aduc78875@gmail.com>
2025-11-27 01:47:49 +0700

    first commit

---
+++ source/Courses.js
@@ -0,0 +1,40 @@
+const logo = document.querySelector('.navbar1')
+    logo.addEventListener('click', function() {
+        location.reload();
+})
+
+const userAvatar = document.getElementById('userAvatar');
+const dropdownMenu = document.getElementById('dropdownMenu');
+const btnProfile = document.getElementById('btnProfile');
+const btnLogout = document.getElementById('btnLogout');
+
+userAvatar.addEventListener('click', function(e) {
+    e.stopPropagation(); // Dừng lan truyền , chỉ khi click vào avt mới hiện lên menu 
+    dropdownMenu.classList.toggle('active');//
+});
+
+document.addEventListener('click', function(e) {
+    if (!userAvatar.contains(e.target) && !dropdownMenu.contains(e.target)) {
+        dropdownMenu.classList.remove('active');
+    }
+});
+
+btnProfile.addEventListener('click', function() {
+    alert('Xem thông tin cá nhân');
+    dropdownMenu.classList.remove('active');
+});
+
+btnLogout.addEventListener('click', function() {
+    const confirmed = confirm('Bạn có chắc muốn đăng xuất?');
+    if (confirmed) {
+        alert('Đăng xuất thành công!');
+        dropdownMenu.classList.remove('active');
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+        dropdownMenu.classList.remove('active');
+    }
+}); 
+
